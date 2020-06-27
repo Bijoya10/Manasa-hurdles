57bbb714a0ad113962f7266b0963ed1283129a0a
Manasa1010 <64597788+Manasa1010@users.noreply.github.com>
2020-06-27 23:10:27 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,36 @@
+class Form{
+    constructor(){
+        this.input=createInput('name');
+        this.button=createButton('play');
+        this.greeting=createElement('h3');
+    }
+    hide(){
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+    }
+    dispaly(){
+        var title=createElement('h2');
+        title.html("Car Racing Game");
+        title.position(displayWidth/2-50,0);
+        
+      
+        this.input.position(displayWidth/2-50,displayHeight/2-50);
+       this.button.position(displayWidth/2-50,displayHeight/2);
+       this.reset.position(displayWidth-250,displayHeight/8);
+
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+            player.name=this.input.value();
+
+            playerCount++;
+            player.index=playerCount;
+            player.update();
+            player.updateCount(playerCount);
+            
+            this.greeting.html("hello "+player.name);
+           this.greeting.position(displayWidth/2-50,displayHeight/4);
+        });
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canavs;
+var database;
+var playerCount;
+var gameState=0
+var form,player,game;
+var allPlayers;
+
+
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    database=firebase.database();
+    
+}
+
+
+function draw() {
+    
+
+}
+
